a2030a95ef5aef7a0bc8c0770d85eedcc827fc6f
LA0920 <lovealex.hess@gmail.com>
2025-02-06 12:24:23 -0500

    Update main.py
    Changed wordage in function definition

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a: object, b: object) -> None:
     if a == b:
-        print('Test passed')
+        print('PASS')
     else:
-        print('Test failed')
+        print('FAIL')
 
 
 in_autotests_we_trust(10, '10')
